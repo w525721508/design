5f7c3196c98ce380416c7ab71fd9c3319683adf8
tzduan <tzduan@Ctrip.com>
2018-02-01 15:32:02 +0800

    简单集成Bugly

--- app/src/main/java/design/root/App.java
+++ app/src/main/java/design/root/App.java
@@ -3,6 +3,7 @@ package design.root;
 import android.app.Application;
 
 import com.blankj.utilcode.util.Utils;
+import com.tencent.bugly.crashreport.CrashReport;
 
 /**
  * Created by Administrator on 2018/1/22.
@@ -17,5 +18,12 @@ public class App extends Application {
         instance = this;
         //初始化工具类
         Utils.init(this);
+
+
+        //TODO 注意重新配置APPID '8b0ad7c66a'此为基础项目APPID
+        CrashReport.initCrashReport(getApplicationContext(), "8b0ad7c66a", false);
+
+        //测试Bugly，请勿打开此注释
+//        CrashReport.testJavaCrash();
     }
 }
