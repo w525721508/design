48f80cbaace24106b4b611dfc7551f89049db998
tzduan <tzduan@Ctrip.com>
2018-02-05 10:10:07 +0800

    add TitleView with back event and replace origin title view

--- app/src/main/java/design/root/ui/main/MainModel.java
+++ app/src/main/java/design/root/ui/main/MainModel.java
@@ -4,5 +4,5 @@ package design.root.ui.main;
  * Created by Administrator on 2018/1/22.
  */
 
-public class MainModel extends design.root.ui.main.MainContract.Model {
+public class MainModel extends MainContract.Model {
 }
